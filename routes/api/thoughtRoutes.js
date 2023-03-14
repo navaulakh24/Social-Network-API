34fe8a787ce63cdaa4ac2aa8bc3a62e641ada2ea
Nav <navdeep_aulakh24@hotmail.com>
2023-03-14 15:50:51 -0400

    forgot to add a route to thoughtRoutes.js

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -11,15 +11,22 @@ const {
 } = require('../../controllers/thought-controller');
 
 // /api/thoughts
+// GET all thoughts
+// POST to create a new thought
 router.route('/').get(getAllThoughts).post(createThought);
 
 // /api/thoughts/:id
+// GET one thought by its _id
+// PUT to update a thought by its _id
+// DELETE to remove a thought by its _id
 router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
+// POST to create a reaction stored in a single thought's reactions array field
 router.route('/:thoughtId/reactions').post(addReaction);
 
 // /api/thoughts/:thoughtId/reactions/:reactionId
+// DELETE to pull and remove a reaction by the reaction's reactionId value
 router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
 
 module.exports = router;
