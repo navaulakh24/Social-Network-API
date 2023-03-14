89521670ae7b40dc247d8741b0c77948ce8fffa6
Nav <navdeep_aulakh24@hotmail.com>
2023-03-14 15:16:52 -0400

    did delete user and thought, and also adding a friend

--- controllers/userController.js
+++ controllers/userController.js
@@ -40,4 +40,43 @@ module.exports = {
 
     // PUT - update user by id
     async updateUser(req, res) {
-        
+        try {
+            const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    // DELETE - delete user and thought by id
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findByIdAndDelete(req.params.id);
+            await Thought.deleteMany({ username: user.username });
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    // POST - add a new friend to a user's friend list
+    async addFriend(req, res) {
+        try{
+            const user = await User.findOne({ _id: req.params.userId });
+            const friend = await User.findOne({ _id: req.params.friendId });
+            if (!user || !friend) {
+                res.status(404).json({ message: 'User or friend not found' });
+                return;
+            }
+            if (user.friends.includes(friend._id)) {
+                res.status(400).json({ message: 'Friend already exists' });
+                return;
+            }
+            user.friends.push(friend._id);
+            await user.save();
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }, 
+    
