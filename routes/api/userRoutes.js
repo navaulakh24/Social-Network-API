75bc6f578ff9bcd903dad07c37bcfaf2361e27ec
Nav <navdeep_aulakh24@hotmail.com>
2023-03-14 15:49:12 -0400

    completed user route, added some comments on routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../../controllers/user-controller');
+
+// /api/users
+// GET all users
+// POST a new user
+router.route('/').get(getAllUsers).post(createUser);
+
+// /api/users/:id
+// GET one user by its _id and populated thought and friend data
+// PUT to update a user by its _id
+// DELETE to remove user by its _id
+router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+// POST to add a new friend to a user's friend list
+// DELETE to remove a friend from a user's friend list
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+module.exports = router;
